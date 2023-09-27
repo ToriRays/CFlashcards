2ff70b8a77e1a42372c63edb632d660d12018fad
ToriRays <98709974+ToriRays@users.noreply.github.com>
2023-09-27 14:00:19 +0200

    Text to speech conversion

--- CFlashcards/wwwroot/js/site.js
+++ CFlashcards/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const textarea = document.querySelector("textarea");
+const button = document.querySelector(".speech-button");
+let isSpeaking = false;
+let speechSynthesis = window.speechSynthesis;
+let intervalId;
 
-// Write your JavaScript code.
+const textToSpeech = () => {
+    const text = textarea.value;
+
+    if (isSpeaking) {
+        speechSynthesis.cancel();
+        isSpeaking = false;
+        button.innerText = "Convert to Speech";
+        clearInterval(intervalId);
+        return;
+    }
+
+    if (text && text.length > 0) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(utterance);
+        isSpeaking = true;
+        button.innerText = "Pause";
+    }
+}; 
+
+button.addEventListener("click", textToSpeech);
+
+intervalId = setInterval(() => {
+    if (!speechSynthesis.speaking && isSpeaking) {
+        isSpeaking = false;
+        button.innerText = "Convert to Speech";
+        clearInterval(intervalId);
+    }
+}); // Adjust the interval duration as needed
