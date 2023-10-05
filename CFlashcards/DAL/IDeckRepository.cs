f80fd21b3efb0bba7f066e3495353a7b4b418b68
PiotrKopczynski <piotrkopczynski52@gmail.com>
2023-10-05 14:47:10 +0200

    merge master into Piotr.

--- CFlashcards/DAL/IDeckRepository.cs
+++ CFlashcards/DAL/IDeckRepository.cs
@@ -5,6 +5,7 @@ namespace CFlashcards.DAL
     public interface IDeckRepository
     {
         Task<IEnumerable<Deck>?> SearchDecksByTitle(string flashcardsUserId, string title);
+        // TO DO: Make a function to check if deck Title exists
         Task<IEnumerable<Deck>?> GetAll(string flashcardsUserId);
         Task<Deck?> GetDeckById(int id);
         Task<bool> Create(Deck deck);
